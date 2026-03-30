7fc7303be0343cd85fbad70dad45dd50be788a94
serenity <serenitydedman@gmail.com>
2026-03-30 21:14:40 +0000

    updated index html

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ let animalName = "cat";
 let height = 3.4;
 let isIndoorPet = true;
 
-document.getElementById("result").innerHTML = typeof animalName;
-document.getElementById("numeric").innerHTML = typeof height;
-document.getElementById("boolean").innerHTML = typeof isIndoorPet;
+document.getElementById("result").innerHTML = animalName;
