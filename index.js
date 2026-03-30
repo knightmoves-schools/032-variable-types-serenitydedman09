62a375e5e380bdf2a602c9e09580b69bd5e8209a
serenity <serenitydedman@gmail.com>
2026-03-30 21:09:21 +0000

    updated index

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let animalName = "cat";
+let height = 3.4;
+let isIndoorPet = true;
 
+document.getElementById("string").innerHTML = typeof animalName;
+document.getElementById("numeric").innerHTML = typeof height;
+document.getElementById("boolean").innerHTML = typeof isIndoorPet;
